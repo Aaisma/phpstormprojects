b33236221cb6cc6f1a4adc8b6e4f13d3eaddaaa8
Aaisma Wagle <98641051+Aaisma@users.noreply.github.com>
2022-04-17 23:46:40 +0545

    Update register.php

--- register.php
+++ register.php
@@ -17,10 +17,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
         {
             mysqli_stmt_bind_param($stmt, "s", $param_username);
 
-            // Set the value of param username
+            // Setting the value of param username
             $param_username = trim($_POST['username']);
 
-            // Try to execute this statement
+            // Trying to execute this statement
             if(mysqli_stmt_execute($stmt)){
                 mysqli_stmt_store_result($stmt);
                 if(mysqli_stmt_num_rows($stmt) == 1)
@@ -51,13 +51,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
         $password = trim($_POST['password']);
     }
 
-// Check for confirm password field
+// Checking for confirm password field
     if(trim($_POST['password']) !=  trim($_POST['confirm_password'])){
         $password_err = "Passwords should match";
     }
 
 
-// If there were no errors, go ahead and insert into the database
+// If there are no errors, go ahead and insert into the database
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err))
     {
         $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
@@ -92,7 +92,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
 <!doctype html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -121,9 +120,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
             <li class="nav-item">
                 <a class="nav-link" href="logout.php">Logout</a>
             </li>
-
-
-
         </ul>
     </div>
 </nav>
@@ -159,9 +155,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
         <button type="submit" class="btn btn-primary">Sign in</button>
     </form>
 </div>
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
